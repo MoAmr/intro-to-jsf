142a8ca78b5e7a6bfcf730aa10055931dae71656
Mohammed Amr <haveagoodday.mido@gmail.com>
2020-02-21 21:12:20 +0200

    Using Managed Beans. closes #1

---
+++ src/main/java/com/samples/jsf/bean/SampleSessionScopedBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.samples.jsf.bean;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author mohammed
+ */
+@Named("sampleSessionBean")
+@SessionScoped
+public class SampleSessionScopedBean implements Serializable {
+    
+}
